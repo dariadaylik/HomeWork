b15151ac5592d1e9b693b08d30bd2723bd4724fa
dariadaylik <117258309+dariadaylik@users.noreply.github.com>
2023-03-02 08:38:17 +0300

    Add files via upload

---
+++ hw7/Hw7.java
@@ -0,0 +1,52 @@
+package org.example.hw7;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class Hw7 {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("start-maximized");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://www.21vek.by/");
+
+
+        WebElement webElement1 = driver.findElement(By.xpath("//*[@id=\"modal-cookie\"]/div/div[2]/div/button[2]"));
+        webElement1.click();
+
+        WebElement webElement2 = driver.findElement(By.className("userToolsText"));
+        webElement2.click();
+
+        WebElement webElement3 = driver.findElement(By.xpath("//*[@id=\"userToolsDropDown\"]/div/div[1]/div[2]/button/div/span"));
+        webElement3.click();
+
+        WebElement webElement4 = driver.findElement(By.id("login-email"));
+        webElement4.click();
+        webElement4.sendKeys("prusakovadp@yandex.ru");
+
+        WebElement webElement5 = driver.findElement(By.id("login-password"));
+        webElement5.click();
+        webElement5.sendKeys("128bdcbd");
+
+        WebElement webElement6 = driver.findElement(By.xpath("//*[@id=\"modal\"]/div/div/div[2]/div/div/form/div/div[2]/div[3]/button/div"));
+        webElement6.click();
+
+
+    }
+}
+
+
+
+
---
+++ lesson7/MyTest.java
@@ -0,0 +1,67 @@
+package org.example.lesson7;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class MyTest {
+
+    static WebDriver driver;
+
+    @BeforeAll
+    static void init(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @BeforeEach
+    void getPage(){
+        driver.get("https://www.21vek.by/");
+    }
+
+    @Test
+    void test() throws InterruptedException {
+        WebElement webElement1 = driver.findElement(By.xpath("//*[@id=\"modal-cookie\"]/div/div[2]/div/button[2]"));
+        webElement1.click();
+
+        WebElement webElement2 = driver.findElement(By.className("userToolsText"));
+        webElement2.click();
+
+        WebElement webElement3 = driver.findElement(By.xpath("//*[@id=\"userToolsDropDown\"]/div/div[1]/div[2]/button/div/span"));
+        webElement3.click();
+
+        WebElement webElement4 = driver.findElement(By.id("login-email"));
+        webElement4.click();
+        webElement4.sendKeys("prusakovadp@yandex.ru");
+
+        WebElement webElement5 = driver.findElement(By.id("login-password"));
+        webElement5.click();
+        webElement5.sendKeys("128bdcbd");
+
+        WebElement webElement6 = driver.findElement(By.xpath("//*[@id=\"modal\"]/div/div/div[2]/div/div/form/div/div[2]/div[3]/button/div"));
+        webElement6.click();
+
+        Assertions.assertEquals("Онлайн-гипермаркет 21vek.by",driver.getTitle(),"Не та страница");
+
+
+    }
+
+    @AfterAll
+    static void close(){
+        driver.quit();
+
+
+    }
+
+
+}
